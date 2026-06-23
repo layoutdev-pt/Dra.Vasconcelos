0c7d793d52a87fcab81e2a332274d496ece0d3ba
Miguel Correia <146827960+miguelolegna@users.noreply.github.com>
2026-06-24 00:04:01 +0100

    Remove AcademyMetrics component from Books page

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -10,7 +10,7 @@ export const Books: React.FC = () => {
   return (
     <div className="w-full">
       <AcademyHero />
-      <AcademyMetrics />
+    
 
       <section className="py-24 bg-site-bg" id="catalogo">
         <div className="max-w-[1400px] 2xl:max-w-[1600px] mx-auto px-6 lg:px-8 2xl:px-12">
