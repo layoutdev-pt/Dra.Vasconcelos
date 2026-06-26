1d38cdf2f515c11c9e754cd8c1f763fb6c6e9dbe
miguel olegna <miguel.olegna1@gmail.com>
2026-06-26 15:43:33 +0100

    refactor: link de leadmagnet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Hero } from "../sections/home/Hero";
 import { BooksSection } from "../sections/home/BooksSection";
 import { AcademyCourses } from "../sections/academy/AcademyCourses";
@@ -6,6 +6,18 @@ import { BioReset } from "../components/BioReset";
 import { LeadMagnet } from "../sections/home/LeadMagnet";
 
 export const Home: React.FC = () => {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      setTimeout(() => {
+        const element = document.querySelector(hash);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 500);
+    }
+  }, []);
+
   return (
     <div className="w-full">
       <Hero />
